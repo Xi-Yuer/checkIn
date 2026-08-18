c461e57c2d64074f9f7eb37b6cc4ea4a06daa8d7
wx <2214380963@qq.com>
2026-08-18 15:38:34 +0800

    Initial Commit

---
+++ checkIn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  checkIn
+//
+//  Created by 万雪 on 2026/8/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ checkIn/checkInApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkInApp.swift
+//  checkIn
+//
+//  Created by 万雪 on 2026/8/18.
+//
+
+import SwiftUI
+
+@main
+struct checkInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
